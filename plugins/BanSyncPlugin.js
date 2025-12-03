46c5914d15ab6365318592b5048c176e2b4b0b12
Gary <40074095+secludedhusky@users.noreply.github.com>
2025-12-03 14:18:40 -0500

    Add BanSyncPlugin.js for synchronized ban management
    Initial implementation of Ban Sync Plugin with configuration.

---
+++ plugins/BanSyncPlugin.js
@@ -0,0 +1,19 @@
+/**
+ * FM-DX Webserver Ban Sync Plugin
+ *
+ * Enhances ban functionality with synchronized ban database from GitHub
+ * Provides advanced ban management, export capabilities, and automatic sync
+ *
+ * @author shortcircuit404
+ * @version 1.0.0
+ */
+
+var pluginConfig = {
+    name: 'Ban Sync Plugin',
+    version: '1.0.0',
+    author: 'shortcircuit404',
+    description: 'Advanced ban management with synchronized ban database',
+    frontEndPath: 'BanSyncPlugin/BanSyncPlugin.js'
+};
+
+module.exports = { pluginConfig };
